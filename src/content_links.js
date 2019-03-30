7b34d01ea2bb8c6c3b61438c9427890212ef77a3
furikake6000 <ewave63@keio.jp>
2019-03-30 20:13:50 +0900

    Redesign links page

--- src/content_links.js
+++ src/content_links.js
@@ -1,41 +1,6 @@
 import React from 'react';
 import { Box, Heading, Subhead, Link } from 'rebass';
 
-const LINK_DATA = [
-  {
-    title: 'Imai Laboratory - GitHub',
-    url: 'https://github.com/imai-laboratory'
-  },
-  {
-    title: '計算理論2017 - お魚天国',
-    url: 'http://osakana.ailab.ics.keio.ac.jp'
-  },
-  {
-    title: '『心と脳－認知科学入門』コメント・参考文献',
-    url: 'http://www.ayu.ics.keio.ac.jp/references/cognitivescience/'
-  },
-  {
-    title: 'Cognitive Robot Interaction 参考文献',  // TODO: Fix title
-    url: 'http://www.ailab.ics.keio.ac.jp/webpage_personal/cognitive_robot_interaction/'
-  },
-];
-
-function createLinkList() {
-  // Create current member list
-  var linkList = [];
-  LINK_DATA.forEach((data, index) => {
-    var link = <Link href={data.url} children={data.title} />;
-    if (data.option === 'invalid') {
-      link = (<strike>{link}</strike>);
-    }
-    linkList.push(
-      <Box width={1} key={index} p={3}>
-        <Subhead>{link}</Subhead>
-      </Box>);
-  });
-  return linkList;
-}
-
 // -----------------------------------------------------------------------------
 // --------------------------------- Component ---------------------------------
 // -----------------------------------------------------------------------------
@@ -47,10 +12,46 @@ export class ContentLinks extends React.Component {
   }
   render() {
     return (
-      <Box width={1}>
-        <Heading pt={3}>{this.props.texts['links_head']}</Heading>
-        {createLinkList()}
-      </Box>
+      <div id="content_links">
+        <section className="hero is-small is-primary is-bold">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">
+                {this.props.texts['links_head']}
+              </h1>
+            </div>
+          </div>
+        </section>
+        <div className="container">
+          <ContentLink title="Imai Laboratory - GitHub" url="https://github.com/imai-laboratory"/>
+          <ContentLink title="計算理論2017 - お魚天国" url="http://osakana.ailab.ics.keio.ac.jp"/>
+          <ContentLink title="『心と脳－認知科学入門』コメント・参考文献" url="http://www.ayu.ics.keio.ac.jp/references/cognitivescience/"/>
+          <ContentLink title="Cognitive Robot Interaction 参考文献" url="http://www.ailab.ics.keio.ac.jp/webpage_personal/cognitive_robot_interaction/"/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class ContentLink extends React.Component {
+  render() {
+    return (
+      <div className="card link-card">
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{this.props.title}</p>
+            </div>
+          </div>
+
+          <div className="content">
+            <p>{this.props.text}</p>
+            <a href={this.props.url} className="button is-primary">
+              here
+            </a>
+          </div>
+        </div>
+      </div>
     );
   }
 }
