58c3ef076c43f632c2223a18ca289bb24835ff7c
furikake6000 <ewave63@keio.jp>
2019-04-15 11:54:58 +0900

    "access"ページへのリンクの追加

--- src/headnav.js
+++ src/headnav.js
@@ -6,50 +6,49 @@ export class HeadNavBar extends React.Component {
   render() {
     return (
       <nav id='headnav' className='navbar' role='navigation' aria-label='main navigation'>
-        <div className='container'>
-          <div className='navbar-brand'>
-            <a className='navbar-item' href='/'>
-              <img src='res/title.png' />
-            </a>
+        <div className='navbar-brand'>
+          <a className='navbar-item' href='/'>
+            <img src='res/title.png' />
+          </a>
 
-            <a role='button' className='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='headNavbarExpandArea'>
-              <span aria-hidden='true' />
-              <span aria-hidden='true' />
-              <span aria-hidden='true' />
-            </a>
+          <a role='button' className='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='headNavbarExpandArea'>
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+          </a>
+        </div>
+
+        <div id='headNavbarExpandArea' className='navbar-menu'>
+          <div className='navbar-start'>
+            <NavItem to='/' name={this.props.texts['menu_home']} />
+            <NavItem to='/research' name={this.props.texts['menu_research']} />
+            <NavDropDown key='publication' name={this.props.texts['menu_publication']}>
+              <NavItem to='/publication_journal' name={this.props.texts['menu_publication_journal']} />
+              <NavItem to='/publication_international' name={this.props.texts['menu_publication_international']} />
+              <NavItem to='/publication_domestic' name={this.props.texts['menu_publication_domestic']} />
+            </NavDropDown>
+            <NavDropDown key='activity' name={this.props.texts['menu_activity']}>
+              <NavItem to='/activity_award' name={this.props.texts['menu_activity_award']} />
+              <NavItem to='/activity_media' name={this.props.texts['menu_activity_media']} />
+              <NavItem to='/activity_talk' name={this.props.texts['menu_activity_talk']} />
+            </NavDropDown>
+            <NavItem to='/member' name={this.props.texts['menu_member']} />
+            <NavItem to='/access' name={this.props.texts['menu_access']} />
+            <NavItem to='/links' name={this.props.texts['menu_links']} />
           </div>
 
-          <div id='headNavbarExpandArea' className='navbar-menu'>
-            <div className='navbar-start'>
-              <NavItem to='/' name={this.props.texts['menu_home']} />
-              <NavItem to='/research' name={this.props.texts['menu_research']} />
-              <NavDropDown key='publication' name={this.props.texts['menu_publication']}>
-                <NavItem to='/publication_journal' name={this.props.texts['menu_publication_journal']} />
-                <NavItem to='/publication_international' name={this.props.texts['menu_publication_international']} />
-                <NavItem to='/publication_domestic' name={this.props.texts['menu_publication_domestic']} />
-              </NavDropDown>
-              <NavDropDown key='activity' name={this.props.texts['menu_activity']}>
-                <NavItem to='/activity_award' name={this.props.texts['menu_activity_award']} />
-                <NavItem to='/activity_media' name={this.props.texts['menu_activity_media']} />
-                <NavItem to='/activity_talk' name={this.props.texts['menu_activity_talk']} />
-              </NavDropDown>
-              <NavItem to='/member' name={this.props.texts['menu_member']} />
-              <NavItem to='/links' name={this.props.texts['menu_links']} />
-            </div>
-
-            <div className='navbar-end'>
-              <div className='navbar-item'>
-                <LanguageSwitcher changeLang={this.props.changeLang.bind(this)} langText={this.props.langText} />
-                <div className='buttons'>
-                  <a href='http://www.ailab.ics.keio.ac.jp/welcome-junior/' className='button is-primary'>
-                    <strong>{this.props.texts['menu_b3']}</strong>
-                  </a>
-                  <a href='https://twitter.com/imailab' className='button is-info'>
-                    <span className='icon'>
-                      <i className='fab fa-twitter' />
-                    </span>
-                  </a>
-                </div>
+          <div className='navbar-end'>
+            <div className='navbar-item'>
+              <LanguageSwitcher changeLang={this.props.changeLang.bind(this)} langText={this.props.langText} />
+              <div className='buttons'>
+                <a href='http://www.ailab.ics.keio.ac.jp/welcome-junior/' className='button is-primary'>
+                  <strong>{this.props.texts['menu_b3']}</strong>
+                </a>
+                <a href='https://twitter.com/imailab' className='button is-info'>
+                  <span className='icon'>
+                    <i className='fab fa-twitter' />
+                  </span>
+                </a>
               </div>
             </div>
           </div>
