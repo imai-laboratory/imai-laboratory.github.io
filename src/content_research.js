4f2cc6d4a12f4c6d7f8a815e635e70d331d7f09e
Akahor1 <akahori@ailab.ics.keio.ac.jp>
2024-02-05 13:31:47 +0900

    add 2023

--- src/content_research.js
+++ src/content_research.js
@@ -66,11 +66,13 @@ export class ContentResearch extends React.Component {
               <img src={UiImg} />
             </div>
           </ResearchSubSection>
+          /*
           <ResearchSubSection text={this.props.texts['research_ui_exp2']}>
             <div className='column has-text-centered'>
               <iframe width="560" height="315" src="https://www.youtube.com/embed/4doyLws0zC0?si=39YIJRieC8d1EIAX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
           </ResearchSubSection>
+          */
         </ResearchSection>
 
         <ResearchSection key='ag' id='ag' title={this.props.texts['research_ag_head']}>
