f04eb4479519f777ea52be15de6d77d86216e699
babahibiki <baba@ailab.ics.keio.ac.jp>
2026-04-09 17:40:37 +0900

    JKAのバナー追加

--- src/footer.js
+++ src/footer.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
 import MadeWithBulmaImg from '../res/made-with-bulma.png';
+import KeirinBannerImg from '../res/keirin-banner.svg';
 
 export class Footer extends React.Component {
   render() {
     return (
-      <footer className='footer'>
-        <div className='content has-text-centered'>
-          <div>
-            {this.props.children}
-          </div>
+      <footer className='footer imai-footer'>
+        <div className='content'>
+          <div className='imai-footer-layout'>
+            <div className='imai-footer-copy has-text-centered'>
+              <div>
+                {this.props.children}
+              </div>
 
-          <a href='https://bulma.io'>
-            <img src={MadeWithBulmaImg} alt='Made with Bulma' width='128' height='24' />
-          </a>
+              <a href='https://bulma.io' className='imai-footer-bulma'>
+                <img src={MadeWithBulmaImg} alt='Made with Bulma' width='128' height='24' />
+              </a>
+            </div>
+
+            <a
+              href='https://keirin.jp/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='imai-footer-banner'
+            >
+              <img src={KeirinBannerImg} alt='KEIRIN.JP Official Website' />
+            </a>
+          </div>
         </div>
       </footer>
     );
